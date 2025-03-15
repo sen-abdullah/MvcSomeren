1d10d30c8738cf5d6841682fa3eef93d2958b6f2
Abdullah SEN <735987@student.inholland.nl>
2025-03-15 22:56:48 +0100

    Add git ignore

--- MvcSomeren/obj/Debug/net8.0/MvcSomeren.AssemblyInfo.cs
+++ MvcSomeren/obj/Debug/net8.0/MvcSomeren.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcSomeren")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a692a8dcb823a37d8880da5628d5b9353827eb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec4a9668354b1ac0ddd72e7b355ec5765a637383")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcSomeren")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcSomeren")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
