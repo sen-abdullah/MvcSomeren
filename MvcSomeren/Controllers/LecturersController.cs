4a7c8514563f0cbfea9ec9be585b023bc984453f
Abdullah SEN <735987@student.inholland.nl>
2025-03-22 02:55:29 +0100

    Improve lecturer implementation

--- MvcSomeren/Controllers/LecturersController.cs
+++ MvcSomeren/Controllers/LecturersController.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using MvcSomeren.Models;
 using MvcSomeren.Repositories;
--- MvcSomeren/Repositories/ILecturersRepository.cs
+++ MvcSomeren/Repositories/ILecturersRepository.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using MvcSomeren.Models;
 
 namespace MvcSomeren.Repositories;
