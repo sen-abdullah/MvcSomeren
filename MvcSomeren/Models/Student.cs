c60c820ebd0eab33c8397ee0e27aa80720b1c67d
SpicyCarry <736115@student.inholland.nl>
2025-03-24 20:58:20 +0100

    Student impelementation

--- MvcSomeren/Models/Student.cs
+++ MvcSomeren/Models/Student.cs
@@ -6,8 +6,11 @@ public class Student
     public int StudentNumber { get; set; }
     public string StudentFirstName { get; set; }
     public string StudentLastName { get; set; }
-    public string StudentPhoneNumber { get; set; }
+    public int StudentPhoneNumber { get; set; }
     public string StudentClass { get; set; }
+    
+    public bool Voucher { get; set; }
+    public int RoomId { get; set; }
 
 
     //constructors
@@ -16,16 +19,20 @@ public class Student
         this.StudentNumber = 0;
         this.StudentFirstName = "";
         this.StudentLastName = "";
-        this.StudentPhoneNumber = "";
+        this.StudentPhoneNumber = 0;
         this.StudentClass = "";
+        this.Voucher = false;
+        this.RoomId = 0;
     }
 
-    public Student(int studentNumber, string studentFirstName, string studentLastName, string studentPhoneNumber, string studentClass)
+    public Student(int studentNumber, string studentFirstName, string studentLastName, int studentPhoneNumber, string studentClass, bool voucher, int roomId)
     {
         this.StudentNumber = studentNumber;
         this.StudentFirstName = studentFirstName;
         this.StudentLastName = studentLastName;
         this.StudentPhoneNumber = studentPhoneNumber;
         this.StudentClass = studentClass;
+        this.Voucher = voucher;
+        this.RoomId = roomId;
     }
 }
