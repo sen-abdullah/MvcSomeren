3e141d5a9969aa8155eab3fb90ba2a9d58790755
SpicyCarry <736115@student.inholland.nl>
2025-06-07 18:59:28 +0200

    FINAL!

--- MvcSomeren/Models/ManageActivityViewModel.cs
+++ MvcSomeren/Models/ManageActivityViewModel.cs
@@ -4,9 +4,7 @@ public class ManageActivityViewModel
 {
     public int ActivityID { get; set; }
     public Activity Activity { get; set; }
-    
     public List<Supervisor> Supervisors { get; set; }
-    //public List<Supervisor> NonSupervisor { get; set; }
     public List<Lecturer> Lecturers { get; set; }
     public List<Participator> Participators { get; set; }
     public List<Student> Students { get; set; }
